38c4dc82e717a92806cace8e11ef5395469ac170
ssanjose <ssanjose@academic.rrc.ca>
2022-06-21 16:25:31 -0500

    Adjust values components and add initial search method

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,33 @@ import { ThemeProvider } from "@mui/system";
 import { ResultPage } from './Result';
 import { lightTheme } from '../themes/light';
 import { SearchPage } from './Search';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const SearchCar = (vinMethod, reqData, data) => {
+  if (vinMethod) {
+    axios.get(`${process.env.REACT_APP_API_URL}/api/${process.env.API_VERSION}
+        /model?make=${reqData.make}&year=${reqData.year}`)
+      .then(res => {
+        console.log(res.data.message);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+};
 
 function App() {
+  const [vinMethod, setVinMethod] = useState(false);
+
+  // useEffect(() => {
+  //   axios.get(`${process.env.REACT_APP_API_URL}/api/v1/ping`)
+  //     .then(res => {
+  //       console.log(res.data.message);
+  //     }).catch(error => {
+  //       console.log(error);
+  //     })
+  // }, []);
+
   return (
     <ScopedCssBaseline enableColorScheme>
       <ThemeProvider theme={lightTheme}>
@@ -14,8 +39,12 @@ function App() {
           flexDirection: { xs: "column", sm: "row" },
           height: "100vh",
         }}>
-          <SearchPage />
-          <ResultPage />
+          <SearchPage
+            vinMethod={vinMethod}
+            setVinMethod={setVinMethod} />
+          <ResultPage
+            vinMethod={vinMethod}
+            setVinMethod={setVinMethod} />
         </Box>
       </ThemeProvider>
     </ScopedCssBaseline>
