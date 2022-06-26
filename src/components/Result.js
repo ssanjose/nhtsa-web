7a3c7f0d52cae33687a6eaed1732c0ac6a06e431
ssanjose <ssanjose@academic.rrc.ca>
2022-06-25 19:02:43 -0500

    Add tips, separate skeletos, and adjust grid items

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,79 +1,65 @@
 import { Grid, Skeleton, Typography } from "@mui/material";
 import { useState } from "react";
-import { ResultContainer, ResultItem } from "../cssComponents/Result";
+import { ResultContainer, ResultItem, ResultMain } from "../cssComponents/Result";
+import { ModelYearResultSkeleton, VINResultSkeleton } from "./skeletons/ResultSkeleton";
+import { VINTips } from "./tips/SearchTips";
 
-export const ResultPage = ({ vinMethod }) => {
-  const [loading, setLoading] = useState(false);
-
+export const ResultPage = ({ loading, data, vinMethod }) => {
   return (
     <ResultContainer>
-      <Result loading={loading} vinMethod={vinMethod} />
+      <ResultMain>
+        {vinMethod ?
+          <>
+            {loading ? <VINResultSkeleton /> : <VINResult data={data} />}
+          </>
+          :
+          <>
+            {loading ? <ModelYearResultSkeleton /> : <ModelYearResult data={data} />}
+          </>
+        }
+      </ResultMain>
     </ResultContainer>
-  );
+  )
 };
 
-const Result = ({ loading, data, vinMethod }) => {
+const VINResult = ({ data }) => {
   return (
     <>
-      {loading ?
-        <>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              <Skeleton variant="rectangular" width={222} height={56} />
-            </Grid>
-            <Grid item xs={9} />
-            <Grid item xs={3}>
-              <Skeleton variant="rectangular" width={222} height={56} />
-            </Grid>
-            <Grid item xs={9} />
-            <Grid item xs={3}>
-              <Skeleton variant="rectangular" width={222} height={56} />
-            </Grid>
-            <Grid item xs={3}>
-              <Skeleton variant="rectangular" width={222} height={56} />
-            </Grid>
-            <Grid item xs={6} />
-          </Grid>
-        </>
-        :
-        <>
-          <div style={{ padding: '20px 5%' }}>
-            {vinMethod ?
-              <Typography variant="h3" component="h2">Search by VIN</Typography>
-              :
-              <Typography variant="h3" component="h2">Search by Model and Year</Typography>
-            }
-          </div>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              <ResultItem>
-                <Typography variant="h4" component="h4">{data ? data.name : "---"}</Typography>
-              </ResultItem>
-            </Grid>
-            <Grid item xs={9} />
-            <Grid item xs={3}>
-              <ResultItem>
-                <Typography variant="h5" component="h5">VIN:</Typography>
-                {data ? data.vin : "-"}
-              </ResultItem>
-            </Grid>
-            <Grid item xs={9} />
-            <Grid item xs={3}>
-              <ResultItem>
-                <Typography variant="h5" component="h5">Make:</Typography>
-                {data ? data.make : "-"}
-              </ResultItem>
-            </Grid>
-            <Grid item xs={3}>
-              <ResultItem>
-                <Typography variant="h5" component="h5">Model:</Typography>
-                {data ? data.model : "-"}
-              </ResultItem>
-            </Grid>
-            <Grid item xs={6} />
-          </Grid>
-        </>
-      }
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <ResultItem>
+            <Typography variant="h4" component="h4">{data ? data.name : "---"}</Typography>
+          </ResultItem>
+        </Grid>
+        <Grid item xs={7}>
+          <ResultItem>
+            <Typography variant="h5" component="h5">VIN:</Typography>
+            {data ? data.vin : "-"}
+          </ResultItem>
+        </Grid>
+        <Grid item xs={5} />
+        <Grid item xs={4}>
+          <ResultItem>
+            <Typography variant="h5" component="h5">Make:</Typography>
+            {data ? data.make : "-"}
+          </ResultItem>
+        </Grid>
+        <Grid item xs={4}>
+          <ResultItem>
+            <Typography variant="h5" component="h5">Model:</Typography>
+            {data ? data.model : "-"}
+          </ResultItem>
+        </Grid>
+        <Grid item xs={4} />
+      </Grid>
+      <VINTips />
+    </>
+  )
+};
+
+const ModelYearResult = ({ data }) => {
+  return (
+    <>
     </>
   )
 };
