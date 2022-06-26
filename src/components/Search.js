1a98d75a79581a46f060bf1d5fef00a56274ae24
ssanjose <ssanjose@academic.rrc.ca>
2022-06-25 19:03:59 -0500

    Add API handler to call api, add submit handler, bind states

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,76 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import { VINSearchAPI } from "../api/VinAPI";
 import { FormContainer, FormText, MethodToggle, SearchContainer } from "../cssComponents/Search";
 
-export const SearchPage = ({ vinMethod, setVinMethod }) => {
+const APIHandler = async (vinMethod, setData, searchData) => {
+  try {
+    let data;
+    if (vinMethod) {
+      if (!searchData.vin)
+        throw new Error("VIN is required");
+      data = await VINSearchAPI(searchData.vin);
+    } else {
+      if (!searchData.model || !searchData.year)
+        throw new Error("Model and Year are required");
+      // data = await ModelYearSearchAPI(searchData.model, searchData.year);
+    }
+    if (data) {
+      setData(data.model);
+      console.log(data);
+    }
+  }
+  catch (error) {
+    console.log(error.message);
+  };
+};
+
+export const SearchPage = ({ vinMethod, setVinMethod, setLoading, setData }) => {
+  const [vin, setVin] = useState("");
+  const [model, setModel] = useState("");
+  const [year, setYear] = useState("");
+
   const handleSwitchChange = (event) => {
     setVinMethod(event.target.checked);
-    // setLoading(true);
+  };
 
-    // setTimeout(() => {
-    //   setLoading(false);
-    // }, 500);
+  const clearSearch = () => {
+    setVin("");
+    setModel("");
+    setYear("");
+  };
+
+  const SubmitHandler = async (event) => {
+    event.preventDefault();
+    let searchData = {
+      vin: vin,
+      model: model,
+      year: year,
+    }
+
+    setLoading(true);
+    await APIHandler(vinMethod, setData, searchData);
+    setTimeout(() => {
+      clearSearch();
+      setLoading(false);
+    }, 1250);
   };
 
   return (
     <SearchContainer>
+      {vinMethod ?
+        <>
+          <div style={{ padding: '20px 5%' }}>
+            <Typography variant="h3" component="h2">Search by VIN</Typography>
+          </div>
+        </>
+        :
+        <>
+          <div style={{ padding: '20px 5%' }}>
+            <Typography variant="h3" component="h2">Search by Model and Year</Typography>
+          </div>
+        </>
+      }
       <FormContainer component="form">
         <Stack direction="row" spacing={1} alignItems="center">
           <Typography>Make/Model</Typography>
@@ -22,25 +80,39 @@ export const SearchPage = ({ vinMethod, setVinMethod }) => {
           />
           <Typography>VIN</Typography>
         </Stack>
-        {vinMethod ? <VINSearch /> : <ModelYearSearch />}
+        {vinMethod ?
+          <VINSearch vin={vin} setVin={setVin} />
+          :
+          <ModelYearSearch
+            model={model} setModel={setModel}
+            year={year} setYear={setYear}
+          />
+        }
+        <Button color="primary" disableElevation fullWidth variant="contained" onClick={SubmitHandler}>Submit</Button>
       </FormContainer>
     </SearchContainer>
   );
 };
 
-const VINSearch = () => {
+const VINSearch = ({ vin, setVin }) => {
   return (
     <>
-      <FormText variant="filled" label="Search by VIN" />
+      <FormText variant="filled" label="Search by VIN"
+        value={vin}
+        onChange={(e) => setVin(e.target.value)} />
     </>
   )
 };
 
-const ModelYearSearch = () => {
+const ModelYearSearch = ({ model, setModel, year, setYear }) => {
   return (
     <>
-      <FormText variant="filled" label="Model" />
-      <FormText variant="filled" label="Year" />
+      <FormText variant="filled" label="Model"
+        value={model}
+        onChange={(e) => setModel(e.target.value)} />
+      <FormText variant="filled" label="Year"
+        value={year}
+        onChange={(e) => setYear(e.target.value)} />
     </>
   )
 };
